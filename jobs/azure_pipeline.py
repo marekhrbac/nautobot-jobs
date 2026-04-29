ff54b32920afabc746a572432b52f5a2108fac9d
Marek <mhrbac@totalservice.cz>
2026-04-29 09:21:28 +0200

    revert

--- jobs/azure_pipeline.py
+++ jobs/azure_pipeline.py
@@ -1,16 +1,11 @@
 from nautobot.apps.jobs import Job, register_jobs
-from nautobot.apps.jobs import JobButtonReceiver
 import requests
 import os
 
-name = "Import VNET to Azure"
 
-class AzurePipeline(JobButtonReceiver):
+class AzurePipeline(Job):
     
-    class Meta:
-        name = "Import VNET to Azure"
-        
-    def run(self,obj):
+    def run(self):
         url = "https://gitlab.msync.cz/api/v4/projects/3/trigger/pipeline"  
         payload = {
             "token": os.getenv("GITLAB_TRIGGER_TOKEN"),
